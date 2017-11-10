086ec1a148b63e115959359578c811e4962dc803
Draichi <lucasdraichi@gmail.com>
2017-11-10 16:32:35 -0200

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h2>recently jobs</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
@@ -9,8 +9,10 @@
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
       <br>
       <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
     </ul>
-    <h2>Ecosystem</h2>
+    <h2>post job</h2>
     <ul>
       <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
@@ -25,7 +27,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'jobs'
     }
   }
 }
@@ -35,15 +37,18 @@ export default {
 <style scoped>
 h1, h2 {
   font-weight: normal;
+  text-transform: uppercase;
 }
 
 ul {
   list-style-type: none;
+  display: flex;
+  justify-content: center;
   padding: 0;
+  flex-wrap: wrap;
 }
 
 li {
-  display: inline-block;
   margin: 0 10px;
 }
 
